74efa9b1514213a6e629014dbf64d2d5c807a844
Prarthana-git <84001841+Prarthana-git@users.noreply.github.com>
2021-05-19 20:54:35 +0530

    Update empwagecomputation.sh

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-empCheck=$((RANDOM%2))
+empcheck=$((RANDOM%2))
 isPresent=1
 Emp_RATE_PER_HR=20
 EMP_HRS=8
 
-if [ $empCheck -eq $isPresent ]
+if [ $empcheck -eq $isPresent ]
 then
     wage=$(($EMP_RATE_PER_HR*$EMP_HRS))
 else
